f9c0ec7d6dcc61774161b2cbb267aaed28f1fdae
mrexclamationmark <86800445+mrexclamationmark@users.noreply.github.com>
2022-11-06 01:06:58 +0000

    Update tcp_bypass.c

--- net/ipv4/tcp_bypass.c
+++ net/ipv4/tcp_bypass.c
@@ -7,7 +7,15 @@
 #include <linux/module.h>
 #include <net/tcp.h>
 
+void tcp_bypass_cong_control(void){
+return;
+}
+
 void tcp_bypass_cong_avoid(void) {
+return;
+}
+
+void tcp_bypass_undo_cwnd(void) {
 return;
 }
 
@@ -17,8 +25,9 @@ return;
 
 static struct tcp_congestion_ops tcp_bypass __read_mostly = {
 	.ssthresh	= tcp_bypass_ssthresh,
-	.undo_cwnd	= tcp_reno_undo_cwnd,
+	.undo_cwnd	= tcp_bypass_undo_cwnd,
 	.cong_avoid	= tcp_bypass_cong_avoid,
+	.cong_control   = tcp_bypass_cong_control,
 	.owner		= THIS_MODULE,
 	.name		= "bypass",
 };
