f054eed4cc038903a53005701a5cea6478af030f
mrexclamationmark <86800445+mrexclamationmark@users.noreply.github.com>
2022-11-09 21:58:55 +0000

    Update tcp_bypass.c

--- net/ipv4/tcp_bypass.c
+++ net/ipv4/tcp_bypass.c
@@ -8,47 +8,52 @@
 
 /* tcp_congestion_ops function calls. */
 
-u32 tcp_bypass_ssthresh(struct sock *sk) {
-return 0;
+inline u32 tcp_bypass_ssthresh(struct sock *sk) {
+
+	return 0;
 }
 
-void tcp_bypass_cong_avoid(struct sock *sk, u32 ack, u32 acked) {
+inline void tcp_bypass_cong_avoid(struct sock *sk, u32 ack, u32 acked) {
 }
 
-void tcp_bypass_set_state(struct sock *sk, u8 new_state) {
+inline void tcp_bypass_set_state(struct sock *sk, u8 new_state) {
 }
 
-void tcp_bypass_cwnd_event(struct sock *sk, enum tcp_ca_event ev) {
+inline void tcp_bypass_cwnd_event(struct sock *sk, enum tcp_ca_event ev) {
 }
 
-void tcp_bypass_in_ack_event(struct sock *sk, u32 flags) {
+inline void tcp_bypass_in_ack_event(struct sock *sk, u32 flags) {
 }
 
-void tcp_bypass_pkts_acked(struct sock *sk, const struct ack_sample *sample) {
+inline void tcp_bypass_pkts_acked(struct sock *sk, const struct ack_sample *sample) {
 }
 
-void tcp_bypass_cong_control(struct sock *sk, const struct rate_sample *rs) {
+inline void tcp_bypass_cong_control(struct sock *sk, const struct rate_sample *rs) {
 }
 
-u32 tcp_bypass_undo_cwnd(struct sock *sk) {
-return 0;
+inline u32 tcp_bypass_undo_cwnd(struct sock *sk) {
+
+	return 0;
 }
 
-u32 tcp_bypass_sndbuf_expand(struct sock *sk) {
-return 0;
+inline u32 tcp_bypass_sndbuf_expand(struct sock *sk) {
+
+	return 0;
 }
 
-size_t tcp_bypass_get_info(struct sock *sk, u32 ext, int *attr, union tcp_cc_info *info) {
-return 0;
+inline size_t tcp_bypass_get_info(struct sock *sk, u32 ext, int *attr, union tcp_cc_info *info) {
+
+	return 0;
 }
 
-void tcp_bypass_init(struct sock *sk) {
+inline void tcp_bypass_init(struct sock *sk) {
 }
 
-void tcp_bypass_release(struct sock *sk) {
+inline void tcp_bypass_release(struct sock *sk) {
 }
 
-static struct tcp_congestion_ops tcp_bypass __read_mostly = {
+static struct tcp_congestion_ops tcp_bypass = {
+	.flags		= TCP_CONG_NON_RESTRICTED,
 	.ssthresh	= tcp_bypass_ssthresh,
 	.cong_avoid	= tcp_bypass_cong_avoid,
 	.set_state	= tcp_bypass_set_state,
@@ -65,12 +70,12 @@ static struct tcp_congestion_ops tcp_bypass __read_mostly = {
 	.name		= "bypass",
 };
 
-static int __init tcp_bypass_register(void)
+inline int __init tcp_bypass_register(void)
 {
 	return tcp_register_congestion_control(&tcp_bypass);
 }
 
-static void __exit tcp_bypass_unregister(void)
+inline void __exit tcp_bypass_unregister(void)
 {
 	tcp_unregister_congestion_control(&tcp_bypass);
 }
