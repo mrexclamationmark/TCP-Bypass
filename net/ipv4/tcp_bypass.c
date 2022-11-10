aeca853a1d45b02746d8b66f2227a0bb0f6a7260
mrexclamationmark <86800445+mrexclamationmark@users.noreply.github.com>
2022-11-10 00:59:52 +0000

    Update tcp_bypass.c

--- net/ipv4/tcp_bypass.c
+++ net/ipv4/tcp_bypass.c
@@ -1,55 +1,59 @@
 /* SPDX-License-Identifier: GPL-2.0-only
-* mrexclamationmarks TCP bypass
-* TCP bypass is a congestion control bypass for linux.
-* using qdiscs is better than congestion control algorithms. */
+ mrexclamationmarks TCP bypass
+ TCP bypass is a congestion control bypass for linux.
+ using qdiscs is better than congestion control algorithms. */
 
 #include <linux/module.h>
 #include <net/tcp.h>
 
 /* tcp_congestion_ops function calls. */
 
-inline u32 tcp_bypass_ssthresh(struct sock *sk) {
+static inline u32 tcp_bypass_ssthresh(struct sock *sk) {
+
+	const struct tcp_sock *tp = tcp_sk(sk);
+
+	return min_t(u32, tp->snd_ssthresh, 0);
+}
+
+static inline void tcp_bypass_cong_avoid(struct sock *sk, u32 ack, u32 acked) {
+}
+
+static inline void tcp_bypass_set_state(struct sock *sk, u8 new_state) {
+}
+
+static inline void tcp_bypass_cwnd_event(struct sock *sk, enum tcp_ca_event ev) {
+}
+
+static inline void tcp_bypass_in_ack_event(struct sock *sk, u32 flags) {
+}
+
+static inline void tcp_bypass_pkts_acked(struct sock *sk, const struct ack_sample *sample) {
+}
+
+static inline void tcp_bypass_cong_control(struct sock *sk, const struct rate_sample *rs) {
+}
+
+static inline u32 tcp_bypass_undo_cwnd(struct sock *sk) {
+
+	const struct tcp_sock *tp = tcp_sk(sk);
+
+	return max_t(u32, tp->snd_cwnd, 100);
+}
+
+static inline u32 tcp_bypass_sndbuf_expand(struct sock *sk) {
 
 	return 0;
 }
 
-inline void tcp_bypass_cong_avoid(struct sock *sk, u32 ack, u32 acked) {
-}
-
-inline void tcp_bypass_set_state(struct sock *sk, u8 new_state) {
-}
-
-inline void tcp_bypass_cwnd_event(struct sock *sk, enum tcp_ca_event ev) {
-}
-
-inline void tcp_bypass_in_ack_event(struct sock *sk, u32 flags) {
-}
-
-inline void tcp_bypass_pkts_acked(struct sock *sk, const struct ack_sample *sample) {
-}
-
-inline void tcp_bypass_cong_control(struct sock *sk, const struct rate_sample *rs) {
-}
-
-inline u32 tcp_bypass_undo_cwnd(struct sock *sk) {
+static inline size_t tcp_bypass_get_info(struct sock *sk, u32 ext, int *attr, union tcp_cc_info *info) {
 
 	return 0;
 }
 
-inline u32 tcp_bypass_sndbuf_expand(struct sock *sk) {
-
-	return 0;
+static inline void tcp_bypass_init(struct sock *sk) {
 }
 
-inline size_t tcp_bypass_get_info(struct sock *sk, u32 ext, int *attr, union tcp_cc_info *info) {
-
-	return 0;
-}
-
-inline void tcp_bypass_init(struct sock *sk) {
-}
-
-inline void tcp_bypass_release(struct sock *sk) {
+static inline void tcp_bypass_release(struct sock *sk) {
 }
 
 static struct tcp_congestion_ops tcp_bypass __read_mostly = {
