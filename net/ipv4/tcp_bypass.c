29f836528470931eb152224afeb397160f5eca72
mrexclamationmark <86800445+mrexclamationmark@users.noreply.github.com>
2022-11-14 19:41:43 +0000

    Update tcp_bypass.c

--- net/ipv4/tcp_bypass.c
+++ net/ipv4/tcp_bypass.c
@@ -38,7 +38,7 @@ static inline u32 tcp_bypass_undo_cwnd(struct sock *sk) {
 
 	const struct tcp_sock *tp = maxu32;
 
-	return tcp_snd_cwnd(tp);
+	return tcp_snd_cwnd(&tp);
 }
 
 static inline u32 tcp_bypass_sndbuf_expand(struct sock *sk) {
